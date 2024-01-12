8b0cee255d39689891a7606347faf81d69443a0f
christophe <chris_je@live.be>
2024-01-12 10:42:41 +0100

    temp template

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,30 @@
 import { ClerkProvider } from "@clerk/nextjs";
+import { Inter, Space_Grotesk } from "next/font/google";
 import "./globals.css";
 import React from "react";
+import type { Metadata } from "next";
+import { ThemeProvider } from "@/context/ThemeProvider";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
+
+export const metadata: Metadata = {
+  title: "DevHelper",
+  description:
+    "A platform to ask questions about programming and let users help solve them with other developers from acros the world",
+  icons: {
+    icon: "/assets/images/site-logo.svg",
+  },
+};
 
 export default function RootLayout({
   children,
@@ -8,10 +32,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body>{children}</body>
-      </html>
-    </ClerkProvider>
+    <html lang="en">
+      <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+        <ClerkProvider
+          appearance={{
+            elements: {
+              formButtonPrimary: "primary-gradient",
+              footerActionLink:
+                "primary-text-gradient hover: text-primary-500 ",
+            },
+          }}
+        >
+          <ThemeProvider>{children}</ThemeProvider>
+        </ClerkProvider>
+      </body>
+    </html>
   );
 }
---
+++ context/ThemeProvider.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import React, { createContext, useContext, useState, useEffect } from "react";
+
+interface ThemeContextType {
+  mode: string;
+  setMode: (mode: string) => void;
+}
+
+const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
+
+export function ThemeProvider({ children }: { children: React.ReactNode }) {
+  const [mode, setMode] = useState("");
+
+  const handleThemeChange = () => {
+    mode === "dark"
+      ? (setMode("light"), document.documentElement.classList.add("light"))
+      : (setMode("dark"), document.documentElement.classList.add("dark"));
+  };
+
+  useEffect(() => {
+    handleThemeChange();
+  }, [mode]);
+
+  return (
+    <ThemeContext.Provider value={{ mode, setMode }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
